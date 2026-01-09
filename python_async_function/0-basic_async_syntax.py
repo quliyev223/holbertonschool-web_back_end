3eee7c2ca4efcde325c57ffafbf1917cadd90f4a
quliyev223 <mikayilquliev@gmail.com>
2026-01-09 12:50:40 +0000

    task

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -9,10 +9,10 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """
     Waits for a random delay between 0 and max_delay seconds and returns it.
-    
+
     Args:
         max_delay (int): The upper bound of the random delay.
-        
+
     Returns:
         float: The actual time spent waiting.
     """
