2ed1e76b27f39db90d464d6b0f98881df9a817ad
quliyev223 <mikayilquliev@gmail.com>
2026-01-09 12:25:07 +0000

    task

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
-"""Async generator that yields 10 random numbers asynchronously"""
+"""Generator that yields 10 random numbers with a 1-second pause"""
 
-import asyncio
 import random
-from typing import AsyncGenerator
+import time
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Coroutine that yields a random float between 0 and 10 every second, 10 times"""
+def async_generator() -> Generator[float, None, None]:
+    """
+    Synchronous generator that yields a float between 0 and 10, 10 times
+
+    Return:
+        Generator[float, None, None]: yields random float values
+    """
     for _ in range(10):
-        await asyncio.sleep(1)
+        # Pause execution for 1 second
+        time.sleep(1)
+
+        # Yield a random float between 0 and 10
         yield random.uniform(0, 10)
