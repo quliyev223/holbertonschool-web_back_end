9a43c3860208da61749b0741d80c302431ac405e
quliyev223 <mikayilquliev@gmail.com>
2026-02-02 14:07:11 +0000

    task

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,6 @@
+/*
+ * Concatenates two arrays and a string into a single array using spread syntax.
+ */
 export default function concatArrays(array1, array2, string) {
   return [...array1, ...array2, ...string];
 }
