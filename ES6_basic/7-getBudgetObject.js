a68013647111ee248023917ccbddd7c8fe17fbae
quliyev223 <mikayilquliev@gmail.com>
2026-02-03 10:46:29 +0000

    task

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,13 +1,8 @@
-/**
- * Returns an object representing a budget.
- * Uses ES6 Object Property Value Shorthand.
- */
 export default function getBudgetObject(income, gdp, capita) {
-  // In ES6, if the key name and the variable name are the same,
-  // you can omit the colon and the duplicate name.
+  // ES6 Property Shorthand: shorthand for income: income, etc.
   const budget = {
-    income, // Equivalent to income: income
-    gdp,    
+    income,
+    gdp,
     capita,
   };
 
