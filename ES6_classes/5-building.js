f83ad9f2005cebe3cb46167ae4bc13b9e0c3d75a
quliyev223 <mikayilquliev@gmail.com>
2026-02-08 10:56:22 +0000

    task

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -22,3 +22,15 @@ export default class Building {
     return this._sqft;
   }
 }
+// export default class Building {
+//   constructor(sqft) {
+//     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+//       throw new Error('Class extending Building must override evacuationWarningMessage');
+//     }
+//     this._sqft = sqft;
+//   }
+//
+//   get sqft() {
+//     return this._sqft;
+//   }
+// }
