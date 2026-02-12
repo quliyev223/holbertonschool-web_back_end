c67a037b5d2dba8b6a3e96d469d28fcba663cb9f
quliyev223 <mikayilquliev@gmail.com>
2026-02-12 14:47:01 +0000

    task

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObject = newGrades.filter((grade) => grade.studentId === student.id)[0];
+      return {
+        ...student,
+	grade: gradeObject ? gradeObject.grade : 'N/A'.
+      };
+    });
+}
