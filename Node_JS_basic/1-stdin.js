d0b775695c489076286bc656852581b5688a578f
quliyev223 <mikayilquliev@gmail.com>
2026-02-27 09:12:11 +0000

    task

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,5 +1,7 @@
+// Display the prompt
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+// Listen for data input
 process.stdin.on('readable', function() {
   var chunk = process.stdin.read();
   if (chunk !== null) {
@@ -7,6 +9,7 @@ process.stdin.on('readable', function() {
   }
 });
 
+// Close message when input stream ends
 process.stdin.on('end', function() {
   process.stdout.write('This important software is now closing\n');
 });
